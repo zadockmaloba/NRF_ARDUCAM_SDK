36fd042b58310f084b07485a7cad16b2418b025c
Zadock Maloba <zadock.maloba@stream4.tech>
2024-07-17 14:34:29 +0530

    Configure default SPI pins

--- include/ArducamSpi.h
+++ include/ArducamSpi.h
@@ -12,6 +12,21 @@
 #define __ARDUCAMSPI_H
 
 #include "stdint.h"
+#include <nrf.h>
+#include <nrf_gpio.h>
+#include <nrf_delay.h>
+#include <nrf_drv_spi.h>
+#include <nrfx_spim.h>
+
+#define SPIM_INSTANCE  0 /**< SPIM instance index. */
+static const nrfx_spim_t spim = NRFX_SPIM_INSTANCE(SPIM_INSTANCE); /**< SPIM instance. */
+
+static volatile bool spim_xfer_done; /**< Flag used to indicate that SPIM instance completed the transfer. */
+
+#define SPIM_MISO_PIN  NRF_GPIO_PIN_MAP(0,25)  /**< SPIM MISO signal. */
+#define SPIM_MOSI_PIN  NRF_GPIO_PIN_MAP(0,26)   /**< SPIM MOSI signal. */
+#define SPIM_SCK_PIN   NRF_GPIO_PIN_MAP(0,27)   /**< SPIM SCK signal. */
+#define SPIM_SS_PIN    NRF_GPIO_PIN_MAP(1,8)  /**< SPIM CS signal. */
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,11 +34,9 @@ extern "C" {
 
 void arducamSpiBegin(void);
 uint8_t arducamSpiTransfer(uint8_t);
-void arducamSpiCsOutputMode(int);
-void arducamSpiCsHigh(int);
-void arducamSpiCsLow(int);
-void arducamDelayMs(uint16_t);
-void arducamDelayUs(uint16_t);
+// void arducamSpiCsOutputMode(int);
+// void arducamSpiCsHigh(int);
+// void arducamSpiCsLow(int);
 void arducamSpiTransferBlock(uint8_t*,uint16_t);
 #ifdef __cplusplus
 }
