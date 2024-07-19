23616d525a1b87ea208dc9fc8548f9f07637276e
Zadock Maloba <zadock.maloba@stream4.tech>
2024-07-19 12:15:34 +0530

    Create unit test files

---
+++ test.h
@@ -0,0 +1,13 @@
+#ifndef A8E1C6B4_EF81_43A1_873F_168AC9E554EE
+#define A8E1C6B4_EF81_43A1_873F_168AC9E554EE
+
+#include <nrf.h>
+
+#include "ArducamCamera.h"
+#include "ArducamSpi.h"
+
+#include "debug.h"
+
+void test_hd_jpeg_no_fx_sunny(ArducamCamera* camera);
+
+#endif /* A8E1C6B4_EF81_43A1_873F_168AC9E554EE */
