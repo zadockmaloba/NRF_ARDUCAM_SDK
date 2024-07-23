44c2744754f3c168832b42a948038eb30f688148
Zadock Maloba <malobazadock@gmail.com>
2024-07-23 16:57:34 +0300

    Code cleanup

--- test.c
+++ test.c
@@ -1,8 +1,15 @@
 #include "test.h"
 
-void test_cam_params(ArducamCamera *camera, CAM_IMAGE_MODE mode, CAM_IMAGE_PIX_FMT fmt, CAM_WHITE_BALANCE wb, CAM_COLOR_FX fx, CAM_SHARPNESS_LEVEL sh) {
+void test_cam_params(ArducamCamera *camera, 
+                    CAM_IMAGE_MODE mode, 
+                    CAM_IMAGE_PIX_FMT fmt, 
+                    CAM_WHITE_BALANCE wb, 
+                    CAM_COLOR_FX fx, 
+                    IMAGE_QUALITY qlty,
+                    CAM_SHARPNESS_LEVEL sh) 
+{
     print(LL_PRINT, "********** CAMERA SETTINGS **********\n");
-    print(LL_PRINT, "Mode: %d Fmt: %d WB: %d FX: %d SH: %d\n", mode, fmt, wb, fx, sh);
+    print(LL_PRINT, "Mode: %d, Fmt: %d, WhitBlnce: %d, FX: %d, Qlty: %d, Shrpness: %d\n", mode, fmt, wb, fx, qlty, sh);
     
     //Reset previous camera settings
     reset(camera);
@@ -19,7 +26,7 @@ void test_cam_params(ArducamCamera *camera, CAM_IMAGE_MODE mode, CAM_IMAGE_PIX_F
     setColorEffect(camera, fx);
 
     //Set image quality
-    setImageQuality(camera, LOW_QUALITY);
+    setImageQuality(camera, qlty);
 
     takePicture(camera, mode, fmt);
 
@@ -54,234 +61,6 @@ void test_all_camera_settings(ArducamCamera* camera) {
     for(int l=0; l< CAM_COLOR_FX_SOLARIZE; ++l)
     //for(int m=0; m< CAM_SHARPNESS_LEVEL_8; ++m) 
     {
-        test_cam_params(camera, i, j, k, l, 6);
+        test_cam_params(camera, i, j, k, l, DEFAULT_QUALITY, 6);
     }
-}
-
-void test_hd_jpeg_no_fx_sunny(ArducamCamera* camera) {
-
-    print(LL_PRINT, "********** HD, JPEG, NO FX, SUNNY **********\n");
-    //Reset previous camera settings
-    reset(camera);
-    memset((void*)img_buffer, 0, IMG_BUFF_SIZE * sizeof(img_buffer[0]));
-
-    //Enable autfocus
-    setAutoFocus(camera, 1);
-
-    setAutoWhiteBalance(camera, 1);
-
-    setAutoWhiteBalanceMode(camera, CAM_WHITE_BALANCE_MODE_SUNNY);
-
-    //Disable color effects
-    setColorEffect(camera, CAM_COLOR_FX_NONE);
-
-    //Set image quality
-    setImageQuality(camera, LOW_QUALITY);
-
-    takePicture(camera, CAM_IMAGE_MODE_HD, CAM_IMAGE_PIX_FMT_JPG);
-
-    uint32_t bytes_read = 0;
-
-    do {
-        bytes_read += readBuff(camera, img_buffer, IMG_BUFF_SIZE);
-    } while(camera->receivedLength > 0);
-
-    print(LL_PRINT, "*********  Img Size: %d \n", bytes_read);
-
-    print(LL_PRINT, "*********  Img Data... \n");
-    for (int i=0; i < IMG_BUFF_SIZE; ++i) {
-        SEGGER_RTT_printf(0, "%#02x ", img_buffer[i]);
-    }
-    print(LL_PRINT, "\n");
-
-    print(LL_PRINT, "----------------------------------------------\n");
-}
-
-void test_hd_jpeg_no_fx_cloudy(ArducamCamera* camera) {
-    print(LL_PRINT, "********** HD, JPEG, NO FX, CLOUDY **********\n");
-    //Reset previous camera settings
-    reset(camera);
-    memset((void*)img_buffer, 0, IMG_BUFF_SIZE * sizeof(img_buffer[0]));
-
-    //Enable autfocus
-    setAutoFocus(camera, 1);
-
-    setAutoWhiteBalance(camera, 1);
-
-    setAutoWhiteBalanceMode(camera, CAM_WHITE_BALANCE_MODE_CLOUDY);
-
-    //Disable color effects
-    setColorEffect(camera, CAM_COLOR_FX_NONE);
-
-    //Set image quality
-    setImageQuality(camera, LOW_QUALITY);
-
-    takePicture(camera, CAM_IMAGE_MODE_HD, CAM_IMAGE_PIX_FMT_JPG);
-
-    int bytes_read = 0;
-
-    do {
-        bytes_read += readBuff(camera, img_buffer, IMG_BUFF_SIZE);
-    } while(camera->receivedLength > 0);
-
-    print(LL_PRINT, "*********  Img Size: %d \n", bytes_read);
-
-    print(LL_PRINT, "*********  Img Data... \n");
-    for (int i=0; i < IMG_BUFF_SIZE; ++i) {
-        SEGGER_RTT_printf(0, "%#02x ", img_buffer[i]);
-    }
-    print(LL_PRINT, "\n");
-
-    print(LL_PRINT, "----------------------------------------------\n");
-}
-
-void test_hd_jpeg_no_fx_office(ArducamCamera* camera) {
-    print(LL_PRINT, "********** HD, JPEG, NO FX, OFFICE **********\n");
-    //Reset previous camera settings
-    reset(camera);
-    memset((void*)img_buffer, 0, IMG_BUFF_SIZE * sizeof(img_buffer[0]));
-
-    //Enable autfocus
-    setAutoFocus(camera, 1);
-
-    setAutoWhiteBalance(camera, 1);
-
-    setAutoWhiteBalanceMode(camera, CAM_WHITE_BALANCE_MODE_OFFICE);
-
-    //Disable color effects
-    setColorEffect(camera, CAM_COLOR_FX_NONE);
-
-    //Set image quality
-    setImageQuality(camera, LOW_QUALITY);
-
-    takePicture(camera, CAM_IMAGE_MODE_HD, CAM_IMAGE_PIX_FMT_JPG);
-
-    int bytes_read = 0;
-
-    do {
-        bytes_read += readBuff(camera, img_buffer, IMG_BUFF_SIZE);
-    } while(camera->receivedLength > 0);
-
-    print(LL_PRINT, "*********  Img Size: %d \n", bytes_read);
-
-    print(LL_PRINT, "*********  Img Data... \n");
-    for (int i=0; i < IMG_BUFF_SIZE; ++i) {
-        SEGGER_RTT_printf(0, "%#02x ", img_buffer[i]);
-    }
-    print(LL_PRINT, "\n");
-
-    print(LL_PRINT, "----------------------------------------------\n");
-}
-
-void test_fhd_jpeg_no_fx_sunny(ArducamCamera* camera) {
-    print(LL_PRINT, "********** FHD, JPEG, NO FX, SUNNY **********\n");
-    //Reset previous camera settings
-    reset(camera);
-    memset((void*)img_buffer, 0, IMG_BUFF_SIZE * sizeof(img_buffer[0]));
-
-    //Enable autfocus
-    setAutoFocus(camera, 1);
-
-    setAutoWhiteBalance(camera, 1);
-
-    setAutoWhiteBalanceMode(camera, CAM_WHITE_BALANCE_MODE_SUNNY);
-
-    //Disable color effects
-    setColorEffect(camera, CAM_COLOR_FX_NONE);
-
-    //Set image quality
-    setImageQuality(camera, LOW_QUALITY);
-
-    takePicture(camera, CAM_IMAGE_MODE_FHD, CAM_IMAGE_PIX_FMT_JPG);
-
-    uint32_t bytes_read = 0;
-
-    do {
-        bytes_read += readBuff(camera, img_buffer, IMG_BUFF_SIZE);
-    } while(camera->receivedLength > 0);
-
-    print(LL_PRINT, "*********  Img Size: %d \n", bytes_read);
-
-    print(LL_PRINT, "*********  Img Data... \n");
-    for (int i=0; i < IMG_BUFF_SIZE; ++i) {
-        SEGGER_RTT_printf(0, "%#02x ", img_buffer[i]);
-    }
-    print(LL_PRINT, "\n");
-
-    print(LL_PRINT, "----------------------------------------------\n");
-}
-
-void test_fhd_jpeg_no_fx_cloudy(ArducamCamera* camera) {
-    print(LL_PRINT, "********** FHD, JPEG, NO FX, CLOUDY **********\n");
-    //Reset previous camera settings
-    reset(camera);
-    memset((void*)img_buffer, 0, IMG_BUFF_SIZE * sizeof(img_buffer[0]));
-
-    //Enable autfocus
-    setAutoFocus(camera, 1);
-
-    setAutoWhiteBalance(camera, 1);
-
-    setAutoWhiteBalanceMode(camera, CAM_WHITE_BALANCE_MODE_CLOUDY);
-
-    //Disable color effects
-    setColorEffect(camera, CAM_COLOR_FX_NONE);
-
-    //Set image quality
-    setImageQuality(camera, LOW_QUALITY);
-
-    takePicture(camera, CAM_IMAGE_MODE_FHD, CAM_IMAGE_PIX_FMT_JPG);
-
-    uint32_t bytes_read = 0;
-
-    do {
-        bytes_read += readBuff(camera, img_buffer, IMG_BUFF_SIZE);
-    } while(camera->receivedLength > 0);
-
-    print(LL_PRINT, "*********  Img Size: %d \n", bytes_read);
-
-    print(LL_PRINT, "*********  Img Data... \n");
-    for (int i=0; i < IMG_BUFF_SIZE; ++i) {
-        SEGGER_RTT_printf(0, "%#02x ", img_buffer[i]);
-    }
-    print(LL_PRINT, "\n");
-
-    print(LL_PRINT, "----------------------------------------------\n");
-}
-void test_fhd_jpeg_no_fx_office(ArducamCamera* camera) {
-    print(LL_PRINT, "********** FHD, JPEG, NO FX, OFFICE **********\n");
-    //Reset previous camera settings
-    reset(camera);
-    memset((void*)img_buffer, 0, IMG_BUFF_SIZE * sizeof(img_buffer[0]));
-
-    //Enable autfocus
-    setAutoFocus(camera, 1);
-
-    setAutoWhiteBalance(camera, 1);
-
-    setAutoWhiteBalanceMode(camera, CAM_WHITE_BALANCE_MODE_OFFICE);
-
-    //Disable color effects
-    setColorEffect(camera, CAM_COLOR_FX_NONE);
-
-    //Set image quality
-    setImageQuality(camera, LOW_QUALITY);
-
-    takePicture(camera, CAM_IMAGE_MODE_FHD, CAM_IMAGE_PIX_FMT_JPG);
-
-    uint32_t bytes_read = 0;
-
-    do {
-        bytes_read += readBuff(camera, img_buffer, IMG_BUFF_SIZE);
-    } while(camera->receivedLength > 0);
-
-    print(LL_PRINT, "*********  Img Size: %d \n", bytes_read);
-
-    print(LL_PRINT, "*********  Img Data... \n");
-    for (int i=0; i < IMG_BUFF_SIZE; ++i) {
-        SEGGER_RTT_printf(0, "%#02x ", img_buffer[i]);
-    }
-    print(LL_PRINT, "\n");
-
-    print(LL_PRINT, "----------------------------------------------\n");
 }
--- test.h
+++ test.h
@@ -14,42 +14,15 @@
 static uint8_t temp_img[TMP_IMG_BUFF_SIZE] = {0};
 static uint8_t img_buffer[IMG_BUFF_SIZE] = {0};
 
-//********** ALL IMAGE TESTS *********************/
 void test_all_camera_settings(ArducamCamera* camera);
 
-//********** JPEG IMAGE TESTS *********************/
-void test_hd_jpeg_no_fx_sunny(ArducamCamera* camera);
-void test_hd_jpeg_no_fx_cloudy(ArducamCamera* camera);
-void test_hd_jpeg_no_fx_office(ArducamCamera* camera);
-
-void test_fhd_jpeg_no_fx_sunny(ArducamCamera* camera);
-void test_fhd_jpeg_no_fx_cloudy(ArducamCamera* camera);
-void test_fhd_jpeg_no_fx_office(ArducamCamera* camera);
-
-void test_hd_jpeg_bw_fx_sunny(ArducamCamera* camera);
-void test_hd_jpeg_bw_fx_cloudy(ArducamCamera* camera);
-void test_hd_jpeg_bw_fx_office(ArducamCamera* camera);
-
-void test_fhd_jpeg_bw_fx_sunny(ArducamCamera* camera);
-void test_fhd_jpeg_bw_fx_cloudy(ArducamCamera* camera); 
-void test_fhd_jpeg_bw_fx_office(ArducamCamera* camera);
-
-//********** YUV IMAGE TESTS *********************/
-void test_hd_yuv_no_fx_sunny(ArducamCamera* camera);
-void test_hd_yuv_no_fx_cloudy(ArducamCamera* camera);
-void test_hd_yuv_no_fx_office(ArducamCamera* camera);
-
-void test_fhd_yuv_no_fx_sunny(ArducamCamera* camera);
-void test_fhd_yuv_no_fx_cloudy(ArducamCamera* camera);
-void test_fhd_yuv_no_fx_office(ArducamCamera* camera);
-
-void test_hd_yuv_bw_fx_sunny(ArducamCamera* camera);
-void test_hd_yuv_bw_fx_cloudy(ArducamCamera* camera);
-void test_hd_yuv_bw_fx_office(ArducamCamera* camera);
-
-void test_fhd_yuv_bw_fx_sunny(ArducamCamera* camera);
-void test_fhd_yuv_bw_fx_cloudy(ArducamCamera* camera);
-void test_fhd_yuv_bw_fx_office(ArducamCamera* camera);
+void test_cam_params(ArducamCamera *camera, 
+                    CAM_IMAGE_MODE mode, 
+                    CAM_IMAGE_PIX_FMT fmt, 
+                    CAM_WHITE_BALANCE wb, 
+                    CAM_COLOR_FX fx, 
+                    IMAGE_QUALITY qlty,
+                    CAM_SHARPNESS_LEVEL sh);
 
 
 #endif /* A8E1C6B4_EF81_43A1_873F_168AC9E554EE */
