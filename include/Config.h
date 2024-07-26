00e0431a2065d5edd25dbded8e9a1504fdabb855
Zadock Maloba <malobazadock@gmail.com>
2024-07-26 13:27:52 +0300

    Initialize globalCameraConfig with defualt settings.

--- include/Config.h
+++ include/Config.h
@@ -6,15 +6,22 @@
 #include <ArducamCamera.h>
 
 struct arducam_config_t {
-    uint8_t 
-    mode = 0,
-    format = 0,
-    whitebalance = 0,
-    fx = 0,
-    sharpness = 0;
+    uint8_t mode;
+    uint8_t format;
+    uint8_t whitebalance;
+    uint8_t fx;
+    uint8_t sharpness;
+    uint8_t quality;
 };
 
-static struct arducam_config_t globalCameraConfig;
+static struct arducam_config_t globalCameraConfig = {
+    .mode = CAM_IMAGE_MODE_WQXGA2,
+    .format = CAM_IMAGE_PIX_FMT_JPG,
+    .whitebalance = CAM_WHITE_BALANCE_MODE_SUNNY,
+    .fx = CAM_COLOR_FX_NONE,
+    .sharpness = CAM_SHARPNESS_LEVEL_7,
+    .quality = DEFAULT_QUALITY,
+};
 
 void tui_configure_camera(ArducamCamera *camera);
 
